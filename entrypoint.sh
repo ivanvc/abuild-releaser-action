b8a39b09a8a67825c2e07f7eea2916bb9b941456
Ivan Valdes <iv@a.ki>
2023-07-06 23:23:05 -0700

    Add input

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,32 @@
 
 set -e
 
-# Set RSA keys
-echo "$RSA_PRIVATE_KEY" > ~/.abuild/"$GITHUB_REPOSITORY_OWNER".rsa
-echo "$RSA_PUBLIC_KEY" > ~/.abuild/"$GITHUB_REPOSITORY_OWNER".rsa.pub
-echo PACKAGER_PRIVKEY=\""$HOME"/.abuild/"$GITHUB_REPOSITORY_OWNER".rsa\" >> ~/.abuild/abuild.conf
+# Set abuild configuration
+mkdir -p "$GITHUB_WORKDIR"/.abuild
+cat <<EOF > "$GITHUB_WORKDIR"/.abuild/abuild.conf
+PACKAGER="$INPUT_AUTHOR"
+REPODEST="$HOME/packages"
+PACKAGER_PRIVKEY="$GITHUB_WORKDIR/.abuild/$GITHUB_REPOSITORY_OWNER.rsa"
+EOF
+
+# Load RSA keys
+echo "$RSA_PRIVATE_KEY" > "$GITHUB_WORKDIR/.abuild/$GITHUB_REPOSITORY_OWNER.rsa"
+echo "$RSA_PUBLIC_KEY" > "$GITHUB_WORKDIR/.abuild/"$GITHUB_REPOSITORY_OWNER".rsa.pub"
 
 # Build any directory that contains an APKBUILD file
-cd "$GITHUB_WORKDIR"
 # shellcheck disable=SC2016
 find . -type f -name APKBUILD -exec /bin/sh -c 'cd $(dirname {}); abuild -r' \;
 
 # Clean the repository and checkout the gh-pages branch
-git checkout -- . && git clean -fd
+git checkout -- . && git clean -fd -e packages
 if ! git checkout gh-pages; then
   echo "The branch gh-pages doesn't exist."
   exit 1
 fi
 
 # Copy released files
-cp ~/.abuild/"$GITHUB_REPOSITORY_OWNER".rsa.pub .
-mv -f ~/packages/* .
+cp "$GITHUB_WORKDIR/.abuild/$GITHUB_REPOSITORY_OWNER.rsa.pub" .
+mv -f packages/* .
 
 # Add files and push the branch
 git add . && git commit -m "Update packages"
